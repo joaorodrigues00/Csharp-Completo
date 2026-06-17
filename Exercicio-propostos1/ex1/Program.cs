82d94fff5f4c639d91b4a1e564c6000c88afc31e
João Rodrigues <joao.rodrigues0033@gmail.com>
2026-06-16 22:59:44 -0300

    ex 1 feito

---
+++ Exercicio-propostos1/ex1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Exercise1
+{
+    public static void Main(string[] args)
+    {
+        int n1 = int.Parse(Console.ReadLine());
+        int n2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"SOMA = {n1 + n2}");
+    }
+}
