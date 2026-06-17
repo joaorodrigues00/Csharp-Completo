06af29d174f9d19cb28fbcc739a537edaaacb858
João Rodrigues <joao.rodrigues0033@gmail.com>
2026-06-17 19:41:41 -0300

    ex4 feito

---
+++ Exercicio-propostos1/ex4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+public class Exercise4
+{
+    public static void Main(string[] args)
+    {
+        int empNumber = int.Parse(Console.ReadLine());
+        int empWorkedHours = int.Parse(Console.ReadLine());
+        double empSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine($"NUMBER = {empNumber}");
+        Console.WriteLine($"SALARY = U$ {empWorkedHours * empSalary:F2}", CultureInfo.InvariantCulture);
+    }
+}
